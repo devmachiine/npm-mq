12fcd50f6430dcb096f6bbb0f15f84c328495f1b
unknown <46693400+devmachiine@users.noreply.github.com>
2019-05-11 23:39:28 +0200

    Add/Remove items from queue

--- tests/queue_test.js
+++ tests/queue_test.js
@@ -4,4 +4,13 @@ module.exports = async ({ test, assert, affirm }) => [
     test("new queue has size 0", () => {
         assert(q.size(), 0)
     })
+    , test("enqueue items increases size", () => {
+        q.push("hello")
+        q.push("world")
+        assert(q.size(), 2)
+    })
+    , test("pull from queue returns added items in fifo order", () => {
+        assert(q.pull(), "hello")
+        assert(q.pull(), "world")
+    })
 ]
